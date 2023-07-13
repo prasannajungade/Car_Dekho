69a271703953b8f36ecc5e2e67bc3a6d64d4f75f
prasannajungade <139142041+prasannajungade@users.noreply.github.com>
2023-07-13 14:51:41 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -2,6 +2,6 @@ import os
 
 PORT_NUMBER=8080
 
-MODLE_FILE_PATH =r"static\models\linear_regression_car_dekho.pkl"
+MODLE_FILE_PATH =os.path.join(r"static","models","linear_regression_car_dekho.pkl")
 
-JSON_FILE_PATH =r"static\JSON\car_dekho.json"
+JSON_FILE_PATH =os.path.join(r"static","JSON","car_dekho.json")
